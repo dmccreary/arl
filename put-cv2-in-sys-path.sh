fd024d46d5d797ffd6d0702ca92e148735f05734
Dan McCreary <dan.mccreary@gmail.com>
2020-01-19 10:12:21 -0600

    Added cv2 path fix

---
+++ put-cv2-in-sys-path.sh
@@ -0,0 +1,5 @@
+# if you get an error about cv2 not being found then you can run this command
+# it will copy the "shared object" file into your local environment path
+# you can also verify that this is in your system path by printing the Python sys.path variable
+cp /usr/lib/python3.6/dist-packages/cv2.cpython-36m-aarch64-linux-gnu.so /home/donkey/env/lib/python3.6
+
